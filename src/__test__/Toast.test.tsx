b5f71bac1efc531e18a20e16bdc9bd0ad7130ad2
Joel Sequeria <joel.sequeira421@gmail.com>
2024-03-12 22:01:51 +0530

    Changes

--- src/__test__/Toast.test.tsx
+++ src/__test__/Toast.test.tsx
@@ -3,6 +3,8 @@ import { render } from "@testing-library/react";
 import Toast from "../components/Toast/Toast";
 import { it, expect } from "vitest";
 
+//Test cases
+
 it("renders the Component correctly", () => {
   const { getByTestId } = render(<Toast />);
   const element = getByTestId("example-component");
